6b35989f02a3f49248546cf61e3d804e7e37bd04
Gaurang <gaurang.swarge@gmail.com>
2020-03-22 11:42:43 -0700

    commit

--- eda.py
+++ eda.py
@@ -17,6 +17,7 @@ def setplotStyle(ax, title, xlabel, ylabel):
       ax.set_title(title, fontsize=18)
       ax.set_xticklabels(labels,rotation=45)
       sns.despine(ax=ax)
+      return None
       
       
 
@@ -25,7 +26,7 @@ def prod_groupby(merged_productsdf):
     merged_productsdf.groupBy('department').count().sort(col("count").desc()).show()
     print("Products in top 20 aisles:\n")
     merged_productsdf.groupBy('aisle').count().sort(col("count").desc()).show()
-    
+
     prod_aisles = [item for item in merged_productsdf.groupBy('aisle').count().sort(col("count").desc()).collect()]
     prod_dept = [item for item in merged_productsdf.groupBy('department').count().sort(col("count").desc()).collect()]
     prod_aisles = pd.DataFrame(prod_aisles,columns=['aisle','prod_count'])
@@ -43,13 +44,34 @@ def prod_groupby(merged_productsdf):
     ax = sns.barplot(x=prod_dept['department'],y=prod_dept['prod_count'])
     setplotStyle(ax,"Departments","Departments", "product count")
     plt.savefig('plots/dept_distribution.png')
-    #plt.show()     
+    #plt.show()
+    return None     
 
 def analyse_id(merged_productsdf):
     print(merged_productsdf.show())
+    #sort by id department, aisle
+        #merged_productsdf.printSchema()
+    #converting to IntType for sorting
+    merged_productsdf = merged_productsdf.withColumn("department_id",merged_productsdf["department_id"].cast(IntegerType()))
+    merged_productsdf = merged_productsdf.withColumn("aisle_id",merged_productsdf["aisle_id"].cast(IntegerType()))
+    
+    print("\n Sorting by Department Id: \n")
+    merged_productsdf.groupBy('department_id').count().sort(col("department_id").asc()).show(21)
+    #merged_productsdf.groupBy('aisle').pivot('department').count().show()
 
+    #print("\n Sorting by Aisle Id: \n")
+    #merged_productsdf.groupBy('aisle_id').count().sort(col("aisle_id").asc()).show(134)
 
+    merged_productsdf.groupBy("department","aisle").count().sort(col("count").desc()).show(50)
+    #merged_productsdf.cube("department","aisle").count().sort(col("count").desc()).show(50)
+    #merged_productsdf.rollup("department","aisle").count().sort(col("count").desc()).show(50)
+    
+    #where departments = missing
+    merged_productsdf.filter(merged_productsdf.department == "missing").show()
+    # where departments = null
+    #where aisle = Null
 
+    return None
 
 
 def main():
