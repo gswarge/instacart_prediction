f305cfd42e5e2e605aac5dadaca5d63d435e6976
Gaurang <gaurang.swarge@gmail.com>
2020-04-01 15:06:58 -0700

    code updated

--- src/main/scala/etl/DataProcessing.scala
+++ src/main/scala/etl/DataProcessing.scala
@@ -1,6 +1,9 @@
 package etl
 
 import org.apache.spark.sql.{DataFrame, SparkSession}
+import shapeless.Data
+import spire.std.`package`.string
+import java.io.File
 
 object DataProcessing {
     println("G")
@@ -27,56 +30,83 @@ object DataProcessing {
             "inferSchema", "true").load(
             csvPath
         )
-        print(csvDf)
         csvDf
+    }
+    def getListOfFiles(dir: String):List[File] = {
+        val d = new File(dir)
+        val okFileExtensions = List("csv")
+        d.listFiles.filter(_.isFile).toList.filter { 
+            file => okFileExtensions.exists(file.getName.endsWith(_))
+        }
     }
 
     def loadData () = {
-        
         println ("Loading Data....")
-        val csvPath = List("data/orders.csv","data/aisles.csv","data/departments.csv","data/products.csv","data/order_products_prior.csv")
+        
+        val csvPath = List("data/orders.csv","data/aisles.csv","data/departments.csv","data/products.csv","data/order_products_prior.csv","data/order_products_train.csv")
 
         val ordersDF = spark.read.format("csv").option(
                         "header", "true").option("inferSchema", "true").load(
                             csvPath(0))
         println("Loaded "+ csvPath(0))
+        ordersDF.printSchema()
+
         val aislesDF = spark.read.format("csv").option(
                         "header", "true").option("inferSchema", "true").load(
                             csvPath(1))
         println("Loaded "+ csvPath(1))
+        aislesDF.printSchema()
+
         val departmentsDF = spark.read.format("csv").option(
                         "header", "true").option("inferSchema", "true").load(
                             csvPath(2))
         println("Loaded "+ csvPath(2))
+        departmentsDF.printSchema()
+
         val productsDF = spark.read.format("csv").option(
                         "header", "true").option("inferSchema", "true").load(
                             csvPath(3))
         println("Loaded "+ csvPath(3))
-        val oppDF = spark.read.format("csv").option(
+        productsDF.printSchema()
+
+        var oppDF = spark.read.format("csv").option(
                         "header", "true").option("inferSchema", "true").load(
                             csvPath(4))
         println("Loaded "+ csvPath(4))
+        oppDF.printSchema()
+
+        var optDF = spark.read.format("csv").option(
+                        "header", "true").option("inferSchema", "true").load(
+                            csvPath(5))
+        println("Loaded "+ csvPath(5))
+        optDF.printSchema()
+
+        //combine products with departments and aisles
+        var productDfFinal = mergeData(productsDF,aislesDF, "aisle_id")
+        println("first merge: products and aisles tables...")
+        productDfFinal.printSchema()
+        productDfFinal = mergeData(productDfFinal,departmentsDF,"department_id")
+        println("second merge products and departments...")
+        productDfFinal.printSchema()
+        // Merge orders and order products train & prior with orders table
+        println("Merging order_products_train with orders df")
+        optDF = mergeData(optDF,ordersDF,"order_id")
+        optDF.printSchema()
+        println("Merging order_products_prior with orders df")
+        oppDF = mergeData(oppDF,ordersDF,"order_id")
+        oppDF.printSchema()
+
     }
 
-    // "⁨/Users/eric/Dropbox/SharpestMinds/Gaurang/Mentorship/instacart_prediction/data/order_products__prior.csv"
-
-    def writeToCSV(df: DataFrame, fileName: DataFrame): Unit = {
+    def mergeData(df1: DataFrame,df2: DataFrame, key :String): DataFrame = {
+        println("Merging Data... ")
+    return df1.join(df2, df1(key) === df2(key), joinType="inner")
+    }
+    
+    def writeToCSV(df: DataFrame, fileName: String): Unit = {
         val spark = SparkSession.builder().getOrCreate()
         println("writing dataframe to csv!")
-        df.write.format("com.databricks.spark.csv").option("header", "true").save("mydata.csv")
+        df.write.format("com.databricks.spark.csv").option("header", "true").save(fileName)
     }
 
-    /*
-    def someML(filePath: String): Unit = {
-        val df = readCSV
-
-        ...
-        DF1 + T1 -> DF2 + T2 -> ... -> DFinal
-        ...
-        val mlPipeline = Pipeline(T1, T2, ...)
-        val DFinal = runPipeline(DF1, mlPipeline)
-        
-        writeToCSV(DFinal)
-    }
-    */
 }
--- src/main/scala/main/instacart_main.scala
+++ src/main/scala/main/instacart_main.scala
@@ -12,16 +12,11 @@ object instacart_main extends Serializable{
   def main(args: Array[String]): Unit = {
 
     println("testing main")
+    var dir = "data/"
+    val dataFiles = DataProcessing.getListOfFiles(dir)
+    println("Files:\n" + dataFiles)
     DataProcessing.loadData()
-    val dummy = args(0)
-    /*
-    dummy match {
-      case "GrabData" => DataProcessing.getParquet(args(1))
-      case "PrintCSV" => DataProcessing.readCSV(args(1))
-      case "loadData" => DataProcessing.loadData(args(1))
-      case _ => throw new ClassNotFoundException(s"$dummy class does not exist !")
-    }
-    */
+    DataProcessing.spark.stop()
   }
 }
 
